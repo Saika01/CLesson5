8ee2fa58b902651b708e8f94218498fe4098b2c7
Aoi <naztya2001@bk.ru>
2023-03-11 11:05:33 +0300

    task33

---
+++ Task33/Program.cs
@@ -0,0 +1,46 @@
+﻿// Pадача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr=new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min,max+1);
+    }
+    return arr;
+}
+
+void ArrayPrinted(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i<arr.Length-1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNumber(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num) return true;
+    }
+    return false;
+}
+
+
+int[] array = CreateArrayRndInt(5,-3,9);
+ArrayPrinted(array);
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(FindNumber(array,number)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+
