bd286a1a2d21024aef73da7bfa9d0303dd1e22da
Aoi <naztya2001@bk.ru>
2023-03-14 20:02:33 +0300

    task36

---
+++ Task36/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void ArrayPrinted(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SumUp(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum+=array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Input 3 numbers");
+int resize = Convert.ToInt32(Console.ReadLine());
+int minimum = Convert.ToInt32(Console.ReadLine());
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateArrayRndInt(resize,minimum,maximum);
+ArrayPrinted(newArray);
+
+Console.WriteLine(SumUp(newArray));
